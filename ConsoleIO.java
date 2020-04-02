cfd830b3d9cdaba1015525a48464951c4bad8e6d
alexSam <alexsamalot19@gmail.com>
2020-04-02 09:52:29 -0400

    begin coding basic functionallity

--- ConsoleIO.java
+++ ConsoleIO.java
@@ -4,113 +4,81 @@ import java.util.Scanner;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-//function that prompts a user for a piece of a student's nformation and stores their response in a HashMap.
-        public void studentInfo(String key, Scanner input){
-        System.out.printf("Please enter the student's %s", key);
-        if (input.hasNext())
-        student.put(key, input.next());
-        }
-
-        public void studentPrint(){
-
-        // Print the student's attributes.
-        student.forEach((attribute, value) -> {
-        System.out.println(
-        String.format("The student's %s is %s.", attribute, value));
-        });
-        }
-
-        public void studentConfirm(){
-
-        /* Create a function that accepts a Scanner and a message to print,
-         * and returns the value the user enters for confirmation.
-         *
-         * Refactor the code below to use this function. */
-        String confirmation = "";
-        System.out.print("Is this information correct? (Y/n) ");
-
-
-
-        if (input.hasNext()) {
-        confirmation = input.next();
-
-        if (!confirmation.equals("Y")) {
-        System.out.println("Try reentering the student's information.");
-        continue;
-        } else {
-        // Add the student to our list...
-        students.add(student);
-
-        // ...Reset the confirmation value and prompt the user again...
-        confirmation = "";
-        System.out.print("Would you like to add another student? (Y/n) ");
-
-        if (input.hasNext()) {
-        confirmation = input.next();
-
-        if (confirmation.equals("Y")) {
-        continue;
-        } else {
-
-        // Move this into a function that accepts an ArrayList or Array.
-
-        // Loop through all students in our list...
-        for (HashMap<String, String> _student : students) {
-        _student.forEach((attribute, value) -> {
-        System.out.println(
-        String.format("The student's %s is %s.", attribute, value));
-        });
-        }
-
-        // ...And break.
-        break;
-        }
-        }
-        }
-        }
-
-
-        }
-
- /*
- *   3. Create a function that accepts a list of student HashMaps and prints
- *      all the keys/values of each one.
- *
- */
- public void createStudent(){
-        // Wrap this up into a function that returns a HashMap.
-        HashMap<String, String> student = new HashMap<>();
-        }
 
 public class ConsoleIO {
-
-  public static void main(String[] args) {
-
-    // Initialize the Scanner we'll need, and the list we'll populate.
-    Scanner input = new Scanner(System.in);
-    ArrayList<HashMap<String, String>> students = new ArrayList<>();
-
-    // As long as the user wants to interact with the program...
-    while (true) {
-
-      HashMap<String, String> student = createStudent();
-
-      String[] keys = {"first", "last", "middle initial", "address", "email", "phone number"}
-
-      for (String key : keys){
-
-        studentInfo(String key, Scanner input);
-
-      }
-
-      // ---- PRINT  ----
-      studentPrint();
-
-      studentConfirm();
-
-
-
+    static void studentInfo(String key, Scanner input, Hashmap<String, String> student){
+        System.out.printf("Please enter the student's %s",key);
+        if(input.hasNext()){
+            student.put(key,input.next());
+        }
     }
-  }
+
+    public static void studentPrint(){
+
+        student.forEach((attribute,value)->{
+            System.out.println(
+                    String.format("The student's %s is %s.",attribute,value));
+        });
+    }
+
+    public static String studentConfirm(String message, Scanner input) {
+
+        String confirmation="";
+        System.out.printf("%s (Y/n) ", message);
+
+        if (input.hasNext()){
+            confirmation=input.next();
+        }
+        return studentConfirm;
+    }
+
+    public void createStudent(){
+        // Wrap this up into a function that returns a HashMap.
+        return new HashMap<String, String>();
+    }
+
+
+    public static void main(String[] args) {
+
+        // Initialize the Scanner we'll need, and the list we'll populate.
+        Scanner input = new Scanner(System.in);
+        ArrayList<HashMap<String, String>> students = new ArrayList<>();
+
+        // As long as the user wants to interact with the program...
+        while (true) {
+
+            Hashmap<String, String> student = createStudent();
+
+            String[] keys = {"first", "last", "middle initial", "address", "email", "phone number"};
+
+            for (String key : keys) {
+
+                studentInfo(key, input, student);
+
+            }
+
+            // ---- PRINT  ----
+            studentPrint();
+/*
+            String message = "Is all of this information true?";
+            studentConfirm(message,input);
+
+            if(studentConfirm.equals("Y")){
+                System.out.println("Great thanks bud!");
+            } else {
+                System.out.println("You're spare parts bud!");
+            }
+
+            String message = "Would you like to add another Student";
+            studentConfirm();
+
+            if(studentConfirm.equals("Y")){
+                System.out.println("okay byeee!");
+            } else {
+                System.out.println("You don't want do do that!");
+            }
+*/
+        }
+    }
 
 }
